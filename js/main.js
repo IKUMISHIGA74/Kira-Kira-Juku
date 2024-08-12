3c2c8f98569ef63cc441a4b0bcbf2221651eb2b5
IKUMISHIGA74 <uminomori74@gmail.com>
2024-08-12 21:03:44 +0900

    スペースの追加

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 $(document).ready(function () {
-  if (window.matchMedia('(max-width: 767px)').matches) {
-    $('#js-hamburger-menu').on('click', function () {
-      $('.navigation').toggleClass('open');
-      $('.hamburger-menu').toggleClass('hamburger-menu--open');
-    });
-    $('.navigation__link').on('click', function () {
-      var target = $(this).attr('href');
-      $('html, body').animate({
-        scrollTop: $(target).offset().top
-      }, 500);
-    });
-  }
+    if (window.matchMedia('(max-width: 767px)').matches) {
+        $('#js-hamburger-menu').on('click', function () {
+            $('.navigation').toggleClass('open');
+            $('.hamburger-menu').toggleClass('hamburger-menu--open');
+        });
+        $('.navigation__link').on('click', function () {
+            var target = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: $(target).offset().top
+            }, 500);
+        });
+    }
 });
 
 $(document).ready(function () {
-  if (window.matchMedia('(min-width: 768px)').matches) {
-  $('.navigation__link').on('click', function (event) {
-    event.preventDefault();
-    var target = $(this).attr('href');
-    $('html, body').animate({
-      scrollTop: $(target).offset().top
-    }, 500);
-  });
-}
+    if (window.matchMedia('(min-width: 768px)').matches) {
+    $('.navigation__link').on('click', function (event) {
+        event.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+    });
+    }
 });
 
 
 $(function(){
-  $('.tab-list-item').on('click', function(){
-    let index = $('.tab-list-item').index(this);
-    $('.tab-list-item').removeClass('is-tab-active');
-    $(this).addClass('is-tab-active');
-    $('.tab-contents').removeClass('is-contents-active');
-    $('.tab-contents').eq(index).addClass('is-contents-active');
-  });
+    $('.tab-list-item').on('click', function(){
+        let index = $('.tab-list-item').index(this);
+        $('.tab-list-item').removeClass('is-tab-active');
+        $(this).addClass('is-tab-active');
+        $('.tab-contents').removeClass('is-contents-active');
+        $('.tab-contents').eq(index).addClass('is-contents-active');
+    });
 }); 
